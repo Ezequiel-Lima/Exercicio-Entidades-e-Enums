01d96868cfaa0cd2aea3849003036710146de3ee
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2021-12-17 15:18:48 -0300

    Adicionado Objetos Finais

--- Exercicio 16/Program.cs
+++ Exercicio 16/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exercicio_16.Entities;
+using Exercicio_16.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,15 +20,18 @@ namespace Exercicio_16
             string name = Console.ReadLine();
 
             Console.Write("Level (Junior/MidLevel/Senior): ");
-            string level = Console.ReadLine();
+            WorkerLevel level = (WorkerLevel)Enum.Parse(typeof(WorkerLevel), Console.ReadLine());
 
             Console.Write("Base salary: ");
             double baseSalary = double.Parse(Console.ReadLine());
 
-            Console.Write("How many contracts to this worker? ");
-            int contract = int.Parse(Console.ReadLine());
+            Department dept = new Department(department);
+            Worker worker = new Worker(name, level, baseSalary, dept);
 
-            for (int i = 0; i < contract; i++)
+            Console.Write("How many contracts to this worker? ");
+            int quantityContract = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantityContract; i++)
             {
                 Console.WriteLine($"Enter #{i+1} contract data: ");
                 Console.Write("Date (DD/MM/YYYY): ");
@@ -37,7 +42,19 @@ namespace Exercicio_16
 
                 Console.Write("Duration (hours): ");
                 int durationHour = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valueHour, durationHour);
+                worker.AddContract(contract);
             }
+
+            Console.WriteLine("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
 
             Console.WriteLine("Press any key to finish");
             Console.ReadKey();
