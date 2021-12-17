bdbacf23ac729edd2dfb3e68c992ac34baa43166
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2021-12-17 15:25:53 -0300

    Adicionado Try

--- Exercicio 16/Program.cs
+++ Exercicio 16/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Exercicio_16.Entities;
 using Exercicio_16.Entities.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercicio_16
 {
@@ -12,52 +8,59 @@ namespace Exercicio_16
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter department's name: ");
-            string department = Console.ReadLine();
-
-            Console.WriteLine("Enter worker data: ");
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-
-            Console.Write("Level (Junior/MidLevel/Senior): ");
-            WorkerLevel level = (WorkerLevel)Enum.Parse(typeof(WorkerLevel), Console.ReadLine());
-
-            Console.Write("Base salary: ");
-            double baseSalary = double.Parse(Console.ReadLine());
-
-            Department dept = new Department(department);
-            Worker worker = new Worker(name, level, baseSalary, dept);
-
-            Console.Write("How many contracts to this worker? ");
-            int quantityContract = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < quantityContract; i++)
+            try
             {
-                Console.WriteLine($"Enter #{i+1} contract data: ");
-                Console.Write("Date (DD/MM/YYYY): ");
-                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Enter department's name: ");
+                string department = Console.ReadLine();
 
-                Console.Write("Value per hour: ");
-                double valueHour = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter worker data: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
 
-                Console.Write("Duration (hours): ");
-                int durationHour = int.Parse(Console.ReadLine());
+                Console.Write("Level (Junior/MidLevel/Senior): ");
+                WorkerLevel level = (WorkerLevel)Enum.Parse(typeof(WorkerLevel), Console.ReadLine());
 
-                HourContract contract = new HourContract(date, valueHour, durationHour);
-                worker.AddContract(contract);
+                Console.Write("Base salary: ");
+                double baseSalary = double.Parse(Console.ReadLine());
+
+                Department dept = new Department(department);
+                Worker worker = new Worker(name, level, baseSalary, dept);
+
+                Console.Write("How many contracts to this worker? ");
+                int quantityContract = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < quantityContract; i++)
+                {
+                    Console.WriteLine($"Enter #{i + 1} contract data: ");
+                    Console.Write("Date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Value per hour: ");
+                    double valueHour = double.Parse(Console.ReadLine());
+
+                    Console.Write("Duration (hours): ");
+                    int durationHour = int.Parse(Console.ReadLine());
+
+                    HourContract contract = new HourContract(date, valueHour, durationHour);
+                    worker.AddContract(contract);
+                }
+
+                Console.Write("\nEnter month and year to calculate income (MM/YYYY): ");
+                string monthAndYear = Console.ReadLine();
+                int month = int.Parse(monthAndYear.Substring(0, 2));
+                int year = int.Parse(monthAndYear.Substring(3));
+
+                Console.WriteLine("Name: " + worker.Name);
+                Console.WriteLine("Department: " + worker.Department.Name);
+                Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
+
+                Console.WriteLine("\nPress any key to finish");
+                Console.ReadKey();
             }
-
-            Console.WriteLine("Enter month and year to calculate income (MM/YYYY): ");
-            string monthAndYear = Console.ReadLine();
-            int month = int.Parse(monthAndYear.Substring(0, 2));
-            int year = int.Parse(monthAndYear.Substring(3));
-
-            Console.WriteLine("Name: " + worker.Name);
-            Console.WriteLine("Department: " + worker.Department.Name);
-            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
-
-            Console.WriteLine("Press any key to finish");
-            Console.ReadKey();
+            catch (Exception error)
+            {
+                Console.WriteLine($">{error.Message}");
+            }
         }
     }
 }
